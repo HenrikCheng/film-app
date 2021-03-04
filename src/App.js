c3f3d7817285612f029a1e5d2a0608c9f792c173
Henrik Cheng <henrikcheng@live.se>
2021-03-04 10:31:30 +0100

    added dynamic movie sort

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MoviesListHeading";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=star wars&apikey=d62eb7e1";
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=d62eb7e1`;
     const response = await fetch(url);
     const responseJson = await response.json();
 
@@ -16,11 +21,15 @@ const App = () => {
   };
 
   useEffect(() => {
-    getMovieRequest();
-  });
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
+      <div className="row">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
