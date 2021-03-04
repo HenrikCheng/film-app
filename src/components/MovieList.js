348613253c249a11ffdaf2747034ff445dba1206
Henrik Cheng <henrikcheng@live.se>
2021-03-04 13:49:56 +0100

    display title

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -18,7 +18,7 @@ const MovieList = (props) => {
             // onClick={() => displayDescription(movie)}
             className="description d-flex align-items-center justify-content-center"
           >
-            🔎
+            🔎 {movie.Title}
           </div>
         </div>
       ))}
