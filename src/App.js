e8f3a858f4d6786cedf8a3fc9695e609ac0ac9b7
Henrik Cheng <henrikcheng@live.se>
2021-03-04 14:12:17 +0100

    added more secure api key

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   const [favourites, setFavourites] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=d62eb7e1`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=d62eb7e1`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
